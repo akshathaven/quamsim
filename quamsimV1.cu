707129c9981415a04c3c0c67ee009e1a4456a80f
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-10 09:44:06 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -109,8 +109,8 @@ __global__ void mat_mul(float *d_u, float *d_ip,float *d_op,int qubit)
 		{
 			d_op[i] = (d_u[0] * d_ip[i]) + (d_u[1] * d_ip[i+(1<<qubit)]);
 			d_op[i+(1<<qubit)] = (d_u[2] * d_ip[i]) + (d_u[3] * d_ip[i+(1<<qubit)]);
-			//printf("%f\n",d_op[i]);
-			//printf("%f\n",d_op[i+(1<<2)]);
+			printf("%f\n",d_op[i]);
+			printf("%f\n",d_op[i+(1<<2)]);
 			
 
 		}
@@ -276,7 +276,7 @@ int main(int argc, char *argv[])
 	cudaMemcpy(op,d_op,(count-1)*sizeof(float),cudaMemcpyDeviceToHost);
 	
 	//mat_mul1(u,ip,op,count-1,qubit_oper);
-	for(int j=0;j<count-6;j++){printf("%.3f\n",ip[j]);    }
+	for(int j=0;j<6;j++){printf("%d\n",qubit[j]);    }
     fclose(FP);
 }
 

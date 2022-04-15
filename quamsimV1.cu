e671efb4c17b17a36d3093f37eac4b50ee943247
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-15 11:59:26 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -289,6 +289,11 @@ int main(int argc, char *argv[])
 			op[n]=frag_op[h]; 
 			n++;
 		}
+		for(int h=0;h<64;h++)
+		{
+			printf("%3f",op[n]);
+			
+		}
 	}
 	
 	for(i=0;i<count-6;i++)
@@ -357,7 +362,7 @@ int main(int argc, char *argv[])
 	cudaMemcpy(op,d_op,(count-1)*sizeof(float),cudaMemcpyDeviceToHost);*/
 	
 	//mat_mul1(u,ip,op,count-1,qubit_oper);
-	for(int j=0;j<count-6;j++){printf("%.3f\n",op[j]);    }
+	//for(int j=0;j<count-6;j++){printf("%.3f\n",op[j]);    }
     fclose(FP);
 }
 

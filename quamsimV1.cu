d8db1b30e74e079a144d35ba714cf2c33840daf4
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-16 12:57:33 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -296,7 +296,7 @@ int main(int argc, char *argv[])
 			}
 			for(int d=0;d<64;d++){
 				frag_ip[d]=mod_ip[k];
-				printf("%.3f\n",mod_ip[d]);
+				printf("%.3f\n",mod_ip[k]);
 				k++;
 			}
 		}

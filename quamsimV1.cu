3805f5a22fe58dcc77488a1ed08b001f4c22e0b2
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-16 12:43:22 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -290,7 +290,7 @@ int main(int argc, char *argv[])
 				mod_ip[l]=ip[j];
 				mod_ip[l+1]=ip[j+(1<<qubit[0])];
 				printf("%.3f",mod_ip[l]);
-				printf("%.3f",mod_ip[l+1);
+				printf("%.3f",mod_ip[l+1]);
 				l+=2;
 			
 			}

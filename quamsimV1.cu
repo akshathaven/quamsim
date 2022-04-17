b67cccd25fa120770afb3098cb51d953c8b55ea2
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-16 22:51:25 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -103,8 +103,8 @@ __global__ void mat_mul(float *d_u, float *d_ip,float *d_op,int qubit)
 	        //printf("%d\n",((i>>qubit)&1));
 	__shared__ float s1[64];
 			__shared__ float s2[1];
-		//if(((i >>  qubit) & 1) == 0)
-		//{
+		if(((i >>  0) & 1) == 0)
+		{
 			
 			for(i=0;i<64;i+=2){
 	
@@ -121,7 +121,7 @@ __global__ void mat_mul(float *d_u, float *d_ip,float *d_op,int qubit)
 	
 			//printf("%.3f\n",d_op[i]);
 			       //printf("%.3f\n",d_op[i+(1<<qubit)]);
-		//}
+		}
 }
 
 

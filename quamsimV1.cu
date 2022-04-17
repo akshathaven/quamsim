d0131508100ab05bda57f85be4e5bdb7cd2b5e8c
akshathaven <100987495+akshathaven@users.noreply.github.com>
2022-04-16 23:01:10 -0400

    Update quamsimV1.cu

--- quamsimV1.cu
+++ quamsimV1.cu
@@ -110,10 +110,11 @@ __global__ void mat_mul(float *d_u, float *d_ip,float *d_op,int qubit)
 	
 				s1[2*threadIdx.x]=(d_u[0]*d_ip[i])+(d_u[1]*d_ip[i+1]);
 				s1[2*threadIdx.x+1]=(d_u[2]*d_ip[i])+(d_u[3]*d_ip[i+1]);
+			printf("%d",i);
 	
 			__syncthreads();
-			printf("%f\n",s1[2*threadIdx.x]);
-			printf("%f\n",s1[2*threadIdx.x+1]);
+			//printf("%f\n",s1[2*threadIdx.x]);
+			//printf("%f\n",s1[2*threadIdx.x+1]);
 			
 		
 			d_op[i]=s1[2*threadIdx.x];
